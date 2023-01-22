2f42a4cda9263d4d9e9d958f74cfd6685f0e785a
Rain <113399463+RainResearchLab@users.noreply.github.com>
2023-01-22 17:23:47 -0500

    Update index.jsx

--- src/pages/Landing/Content/index.jsx
+++ src/pages/Landing/Content/index.jsx
@@ -16,21 +16,6 @@ import { AiFillPlusSquare, AiFillMinusSquare } from "react-icons/ai";
 import { GiWoodAxe } from "react-icons/gi";
 
 
-function NFTsDisplay({address, contractAddress}) {
-  const [nftsHeld, setNftsHeld] = useState(1);
-
-  useEffect(() => {
-    async function getNFTsHeldByAddress() {
-      // Connect to the Ethereum blockchain and get the number of NFTs held by the address
-      // using the web3.js library as described in the previous answer
-      const nfts = await contract.methods.balanceOf(address).call();
-      setNftsHeld(nfts);
-    }
-    getNFTsHeldByAddress();
-  }, [address, contractAddress]);
-
-}
-
 const Description = () => {
   return (
     <>
