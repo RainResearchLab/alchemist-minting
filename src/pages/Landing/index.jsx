adc676aca8d667fdb593199cb6a944d06be8e325
Rain <113399463+RainResearchLab@users.noreply.github.com>
2023-01-31 18:51:56 -0500

    Update index.jsx

--- src/pages/Landing/index.jsx
+++ src/pages/Landing/index.jsx
@@ -9,16 +9,7 @@ const Landing = () => {
     <div className="landing ps-4 pe-4 ps-md-5 pe-md-5 flex-fill">
       <Header />
       <Content />
-    </div>
-    <div>
-      import { CrossmintPayButton } from "@crossmint/client-sdk-react-ui";
-    
-    export default function Mint() {
-        return (
-            <CrossmintPayButton
-                clientId="efc2ed34-9321-4264-bdd5-70c112f9306b"
-                mintConfig={{"type":"erc-721","totalPrice":"<SELECTED_PRICE_IN_ETHER>","numberOfTokens":"<NUMBER_OF_NFTS>"}}
-    </div>            
+    </div>           
             />
         );
     }
