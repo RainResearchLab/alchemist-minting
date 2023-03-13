a9405234fcf8536d678307499deecf8dbd9dbcfb
Rain <113399463+RainResearchLab@users.noreply.github.com>
2023-03-13 08:26:02 -0400

    Update index.jsx

--- src/pages/Landing/Content/index.jsx
+++ src/pages/Landing/Content/index.jsx
@@ -79,7 +79,7 @@ const Preview = ({ onConnect }) => {
           </div>
           <div className="mt-1 d-flex align-items-center justify-content-between">
             <div>Total Supply Remaining:</div>
-            <div className="fw-bold">236</div>
+            <div className="fw-bold">233</div>
           </div>
           <div className="mt-1 d-flex align-items-center justify-content-between">
             <div>{"QUANTITY"}</div>
